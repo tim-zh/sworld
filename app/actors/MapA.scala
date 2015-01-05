2b6b4061912fd0c919c533263a47c6c905a138cd
tim-zh <tim.zhlobich@gmail.com>
2015-01-05 23:42:17 +0300

    splitting LocationA start

---
+++ app/actors/MapA.scala
@@ -0,0 +1,9 @@
+package actors
+
+import akka.actor.Actor
+
+class MapA extends Actor {
+	override def receive = {
+		case _ =>
+	}
+}
---
+++ app/actors/MessagingA.scala
@@ -0,0 +1,9 @@
+package actors
+
+import akka.actor.Actor
+
+class MessagingA extends Actor {
+	override def receive = {
+		case _ =>
+	}
+}
