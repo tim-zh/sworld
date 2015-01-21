41fc4827afb2d784617d231de46487cdc359e713
tim-zh <tim.zhlobich@gmail.com>
2015-01-21 15:36:49 +0300

    cell grid in LocationA

--- app/actors/LocationA.scala
+++ app/actors/LocationA.scala
@@ -4,6 +4,7 @@ import akka.actor._
 import models.GameEntity
 import play.libs.Akka
 
+import scala.collection.mutable
 import scala.collection.parallel.mutable.ParMap
 
 object LocationA {
@@ -26,25 +27,36 @@ object LocationA {
 	def create(name: String, dao: ActorRef) = Akka.system().actorOf(Props(classOf[LocationA], dao), name)
 }
 
-class LocationA(dao: ActorRef) extends Actor {
+class LocationA(dao: ActorRef, width: Int, height: Int, cellSize: Int) extends Actor {
 	import actors.LocationA._
 
 	private var actorsMap = ParMap[ActorRef, GameEntity]()
 	private var entitiesMap = ParMap[GameEntity, ActorRef]()
+	private var entitiesGridMap = mutable.Map[GameEntity, mutable.Set[GameEntity]]()
+
+	private val grid: Array[Array[mutable.Set[GameEntity]]] =
+		for (i <- Array(0 until width / cellSize))
+			yield for (j <- Array(0 until width / cellSize))
+				yield mutable.Set[GameEntity]()
 
 	def receive = {
 		case Enter(entity) =>
 			actorsMap += (sender -> entity)
 			entitiesMap += (entity -> sender)
+			updateGrid(entity)
 			context watch sender
 			sender ! GameEntityA.LocationEntered
 
-		case Leave =>
-			entitiesMap -= actorsMap.getOrElse(sender, null)
+		case Leave if actorsMap contains sender =>
+			val entity = actorsMap(sender)
+			updateGrid(entity, true)
+			entitiesMap -= entity
 			actorsMap -= sender
 
-		case Terminated(actor) =>
-			entitiesMap -= actorsMap.getOrElse(actor, null)
+		case Terminated(actor) if actorsMap contains actor =>
+			val entity = actorsMap(actor)
+			updateGrid(entity, true)
+			entitiesMap -= entity
 			actorsMap -= actor
 
 		case SendMessage(to, msg) =>
@@ -67,6 +79,7 @@ class LocationA(dao: ActorRef) extends Actor {
 			if (isMoveAllowed(x, y, entity)) {
 				entity.x = x
 				entity.y = y
+				updateGrid(entity)
 				dao ! DaoA.UpdateEntity(entity)
 				sender ! GameEntityA.MoveConfirmed(x, y)
 			} else
@@ -85,6 +98,34 @@ class LocationA(dao: ActorRef) extends Actor {
  	def getDistance(xy0: (Double, Double), xy1: (Double, Double)) =
  		Math.sqrt((xy0._1 - xy1._1) * (xy0._1 - xy1._1) + (xy0._2 - xy1._2) * (xy0._2 - xy1._2))
 
-	def isMoveAllowed(x: Double, y: Double, entity: GameEntity) =
+	private def isMoveAllowed(x: Double, y: Double, entity: GameEntity) =
 		0 <= x && x <= 500 && 0 <= y && y <= 500 && Math.abs(entity.x - x) <= 10 && Math.abs(entity.y - y) <= 10
+
+	private def updateGrid(entity: GameEntity, isLeaving: Boolean = false) {
+		if (isLeaving) {
+			entitiesGridMap.get(entity) foreach { _ -= entity }
+			entitiesGridMap -= entity
+			return
+		}
+		val oldCell = entitiesGridMap.get(entity)
+		val newCell = getGridCell(entity.x, entity.y)
+		if (newCell.isDefined) {
+			if (oldCell.isDefined) {
+				if (newCell.get == oldCell.get)
+					return
+				oldCell.get -= entity
+			}
+			newCell.get += entity
+			entitiesGridMap.put(entity, newCell.get)
+		}
+	}
+
+	private def getGridCell(x: Double, y: Double): Option[mutable.Set[GameEntity]] = {
+		val i = (x / width).toInt
+		val j = (y / height).toInt
+		if (i >= 0 && j >= 0 && i < grid.length && j < grid(i).length)
+			Some(grid(i)(j))
+		else
+			None
+	}
 }
